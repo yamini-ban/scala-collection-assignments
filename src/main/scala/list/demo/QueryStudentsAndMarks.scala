584a0a39524820c7c194827870f0b627db615e11
yamini-ban <yaminibansal7@gmail.com>
2020-02-11 22:30:28 +0530

    added more solutions

--- src/main/scala/list/demo/QueryStudentsAndMarks.scala
+++ src/main/scala/list/demo/QueryStudentsAndMarks.scala
@@ -11,9 +11,9 @@ class QueryStudentsAndMarks {
     f(param1, param2, param3)
   }
 
-  def query2(subjectId: Int, count: Int, criteria: String): List[(String, Int)] = {
+  /*def query2(subjectId: Int, count: Int, criteria: String): List[(String, Int)] = {
     QueryStudentsAndMarks.marks.
-  }
+  }*/
 
 }
 
@@ -22,7 +22,10 @@ object QueryStudentsAndMarks {
   val marks: List[Marks] = CreateList.createListOfMarks(5, 10)
   val percentageOfStudents: List[Double] = students.foldLeft(List[Double])((result, student) => {
     val mark = marks.filter(_.studentId == student.id)
-    val percent = mark.foldLeft(0.0)((result, markEle) => )
+    val percent = mark.foldLeft(0.0)(
+      (result, markEle) => result + (markEle.marksObtained / mark.length)
+    )
+    result +: List(percent)
   })
 
   val studentCountOnStatusOfSubject: (Int, Int, String) => Int = (subjectId: Int, percentage: Int, status: String) => {
