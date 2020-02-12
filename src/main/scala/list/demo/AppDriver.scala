006ef1770087d0ec4896b250128735b486aad398
yamini-ban <yaminibansal7@gmail.com>
2020-02-12 10:58:33 +0530

    Driver object updated

--- src/main/scala/list/demo/AppDriver.scala
+++ src/main/scala/list/demo/AppDriver.scala
@@ -2,17 +2,27 @@ package list.demo
 
 object AppDriver extends App{
 
-
-
   val queryStudentsAndMarks = new QueryStudentsAndMarks
 
-  val count = queryStudentsAndMarks.query1(3, 95, "fail",QueryStudentsAndMarks.studentCountOnStatusOfSubject)
+  val listsUsingFor = new ListsUsingFor
 
-  val count2 = queryStudentsAndMarks.query1(3, 95, "pass",QueryStudentsAndMarks.studentCountOnStatusOfSubject)
+  val count = queryStudentsAndMarks.query1And2(3, 95, "fail",QueryStudentsAndMarks.studentCountOnStatusOfSubject)
 
-  println(count)
+  val count2 = queryStudentsAndMarks.query1And2(3, 95, "pass",QueryStudentsAndMarks.studentCountOnStatusOfSubject)
 
-  println(count2)
+  println("Fail Count: " + count)
 
+  println("Pass Count: " + count2)
 
+  val list1 = List(2,1,4,3,6,5)
+
+  val list2 = List("a", "b", "c", "d", "e", "f")
+
+  println("Last element of the list with index is: " + listsUsingFor.lastElementWithIndex(list1))
+
+  println("Aggregate list of two lists is: " + listsUsingFor.aggregateList(list1, list2))
+
+  println("Table of list elements is: " + listsUsingFor.tableOfListElements(list1))
+
+  println("Sum and Multiplication of the list elements is: " + listsUsingFor.sumAndMultiplication(list1))
 }
